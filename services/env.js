628f7282d180409dcbcc5cdeef57b011ec89ff6d
Archange Malonga <malongaarchange242@gmail.com>
2026-05-22 11:47:46 +0100

    Mise à jour

--- services/env.js
+++ services/env.js
@@ -14,7 +14,7 @@ const ENV = {
     // ============================================
     // ORIENTATION SERVICES
     // ============================================
-    PROA_SERVICE_URL: 'https://universearch-proa-service.onrender.com',
+    PROA_SERVICE_URL: 'https://universearch-proa-service-weza.onrender.com',
     PORA_SERVICE_URL: 'https://universearch-pora-service.onrender.com',
 
     // ============================================
